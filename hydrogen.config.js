2cb11288880fb6b2e86380c3857109db218a237d
Rohit Saroha <rohitsaroha@mobikasa.com>
2022-09-08 18:22:44 +0530

    new changes

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'reequilhydrogen.myshopify.com',
+    storefrontToken: 'dd308376f624cfef0c565db76d2d8ee0',
     storefrontApiVersion: '2022-07',
   },
 });
